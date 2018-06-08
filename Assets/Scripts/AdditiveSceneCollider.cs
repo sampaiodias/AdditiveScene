0faf1f43fef243980c7437480c4acb6d55adb25e
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-06-08 16:26:37 -0300

    Added customizable gizmos to AdditiveSceneCollider

--- Assets/Scripts/AdditiveSceneCollider.cs
+++ Assets/Scripts/AdditiveSceneCollider.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(BoxCollider))]
 public class AdditiveSceneCollider : MonoBehaviour
 {
     public string tagToCheck = "Player";
@@ -37,4 +38,21 @@ public class AdditiveSceneCollider : MonoBehaviour
     {
         checking = true;
     }
+
+#if UNITY_EDITOR
+    public Color unselectedGizmoColor = new Color(0.1f, 1.0f, 0.1f, .3f);
+    public Color selectedGizmoColor = new Color(0.1f, 1.0f, 0.1f, .6f);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = unselectedGizmoColor;
+        Gizmos.DrawWireCube(transform.position, GetComponent<Collider>().bounds.size);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = selectedGizmoColor;
+        Gizmos.DrawCube(transform.position, GetComponent<Collider>().bounds.size);
+    }
+#endif
 }
